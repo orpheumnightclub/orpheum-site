2523af46cf7d979842cc4d1444b43fa01026d15f
orpheumnightclub <toxata1@gmail.com>
2026-06-18 18:27:25 +0300

    dopil9

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ var galleryAlbums = [
         dayName: 'П\'ятниця',
         name: 'День Фермера',
         folder: '2026-06-19',
-        images: ['https://res.cloudinary.com/deiitwupc/image/upload/v1781790286/orpheum/images/2026-06-19/2026-06-19.png', 'https://res.cloudinary.com/deiitwupc/image/upload/v1781790289/orpheum/images/2026-06-19/2026-06-19_2.png', 'https://res.cloudinary.com/deiitwupc/image/upload/v1781790292/orpheum/images/2026-06-19/2026-06-19_3.png'],
+        images: ['https://res.cloudinary.com/deiitwupc/image/upload/v1781790286/orpheum/images/2026-06-19/2026-06-19.png', 'https://res.cloudinary.com/deiitwupc/image/upload/v1781790292/orpheum/images/2026-06-19/2026-06-19_3.png'],
         poster: 'https://res.cloudinary.com/deiitwupc/image/upload/v1781790284/orpheum/images/2026-06-19/poster/2026.06.19.png'
     }
 ];
